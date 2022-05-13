f1589765341f7311d26f66f205311a675af66240
Adrian Hatfield <adrianhatfield23@gmail.com>
2022-05-13 15:38:06 +0100

    Create playRound function

--- script.js
+++ script.js
@@ -3,17 +3,40 @@ function computerPlay() {
     //console.log(computerResult);
 
     if (computerResult === 1) {
-        computerResult = "Rock";
+        computerResult = "rock";
     };
     
     if (computerResult === 2) {
-        computerResult = "Paper";
+        computerResult = "paper";
     };
     
     if (computerResult === 3) {
-        computerResult = "Scissors";
+        computerResult = "scissors";
     };
     //console.log(computerResult);
 
     return computerResult;
 };
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection.toLowerCase();
+    //console.log(playerSelection);
+    //console.log(computerSelection);
+
+    let roundResult = "";
+
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        roundResult = "You win! Rock beats scissors.";
+    } else if (playerSelection == "rock" && computerSelection == "rock") {
+        roundResult = "You tie!";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        roundResult = "You lose! Paper beats rock.";
+    }
+
+    //console.log(roundResult);
+    return roundResult;
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
